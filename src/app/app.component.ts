213c9f42650d1390e0872711cac41bdf19b585ac
GkIgor <igorf0660@gmail.com>
2024-06-06 19:00:55 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component, HostListener } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'test-no-reload';
+	title = "test-no-reload";
+
+	@HostListener("window:beforeunload", ["$event"])
+	handleBeforeUnload(event: BeforeUnloadEvent) {
+		event.preventDefault();
+	}
 }
